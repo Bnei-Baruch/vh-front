b7f47fc5378eb1fdb55d189fddc1ebb493cddd11
Yasha <738693-yashasolutions@users.noreply.gitlab.com>
2020-06-23 04:40:13 +0200

    redux config

--- src/App.js
+++ src/App.js
@@ -1,17 +1,22 @@
 import React from 'react'
-import Routes from './Routes'
+import Routes from 'config/Routes'
 import { Provider } from 'react-redux'
-import { ConnectedRouter } from 'connected-react-router'
+
+// Material UI
+import CssBaseline from '@material-ui/core/CssBaseline';
+import { ThemeProvider } from '@material-ui/core/styles';
+import theme from 'config/Theme';
 
 
 function App(props) {
-  const { store, history } = props
+  const { store } = props
 
   return (
     <Provider store={store} >
-      <ConnectedRouter history={history}>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
         <Routes />      
-      </ConnectedRouter>      
+      </ThemeProvider>
     </Provider> 
   );
 }
