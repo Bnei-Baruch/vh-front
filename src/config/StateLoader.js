f0c7e052dd1816b37187a534664d09206c18dbad
Yasha <738693-yashasolutions@users.noreply.gitlab.com>
2020-06-24 22:30:19 +0200

    global state loader

---
+++ src/config/StateLoader.js
@@ -0,0 +1,34 @@
+import React, {useEffect} from 'react'
+import {/*useSelector,*/ useDispatch} from 'react-redux'
+import {setLanguage, setCurrency} from 'redux/actions/i18nActions'
+
+function StateLoader(props) {
+  // const lang = useSelector(state => state.i18n.lang)
+  // const currency = useSelector(state => state.i18n.currency)
+  const dispatch = useDispatch()
+
+  useEffect(() => {
+    const defaultLang = localStorage.getItem('VH_DEFAULT_LANG');
+    const defaultCurrency= localStorage.getItem('VH_DEFAULT_CURRENCY');
+    // Using ISO 2 Letter Language Codes
+    if (defaultLang && defaultLang.length === 2) {
+      dispatch(setLanguage(defaultLang))
+    } else {
+      // Geo IP magic
+    }
+    if (defaultCurrency && defaultCurrency.length === 3) {
+      setCurrency(defaultCurrency)
+    } else {
+      // Geo IP magic
+    }
+    
+  }, [dispatch])
+
+  return (
+    <>
+      {props.children}    
+    </>
+  )
+}
+
+export default StateLoader
