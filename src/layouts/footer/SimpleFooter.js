fc839b2ded699534d10df29a9adff1a1f6fa1f3e
Yasha <738693-yashasolutions@users.noreply.gitlab.com>
2020-06-24 04:24:35 +0200

    basic footer

--- src/layouts/footer/SimpleFooter.js
+++ src/layouts/footer/SimpleFooter.js
@@ -1,10 +1,39 @@
 import React from 'react'
-import {Box} from '@material-ui/core'
+
+import { makeStyles } from '@material-ui/core/styles'
+import I18 from 'components/I18'
+import { Link } from 'react-router-dom'
+import {
+  AppBar,
+  Toolbar,
+  Typography,
+  Button,
+  Box
+} from '@material-ui/core'
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+    display: 'block'
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  bar:{
+    justifyContent: 'space-between'
+  },
+  title: {
+    flexGrow: 1,
+  },
+}));
 
 function SimpleFooter() {
+  const classes = useStyles()
   return (
-    <Box>
-      Links here      
+    <Box className={classes.root}>
+      <Typography>
+        <Link to="/tos"><I18>product.tos</I18></Link>
+      </Typography>
     </Box>
   )
 }
