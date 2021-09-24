d5f3950487021440afa04e23f1e3120584121c23
Yash Shah <yashshh123@gmail.com>
2021-09-24 12:06:36 +0530

    Updated changes for language
    Updated changes for language

--- src/routes/Routes.js
+++ src/routes/Routes.js
@@ -5,6 +5,8 @@ import { dashboardLayoutRoutes, authLayoutRoutes } from "./index";
 import DashboardLayout from "../layouts/Dashboard";
 import AuthLayout from "../layouts/Auth";
 import Page404 from "../pages/auth/Page404";
+import { useTranslation } from "react-i18next";
+import { setDirection } from "utils";
 
 const childRoutes = (Layout, routes) =>
   routes.map(({ children, path, component: Component }, index) =>
@@ -37,8 +39,21 @@ const childRoutes = (Layout, routes) =>
     )
   );
 
-const Routes = () => (
-  <Router>
+const Routes = () => {
+  let query = new URLSearchParams(window.location.search);
+  const { i18n } = useTranslation('common');
+  React.useEffect(() => {
+    let language = query.get('lang') || localStorage.getItem('VH_LANG');
+    if (language) {
+      localStorage.setItem('VH_LANG', language.toLowerCase());
+      i18n.changeLanguage(language.toLowerCase());
+      if (language.toUpperCase() === 'IL') {
+        setDirection('rtl');
+      }
+    }
+  }, [])
+
+  return <Router>
     <Switch>
       {childRoutes(DashboardLayout, dashboardLayoutRoutes)}
       {childRoutes(AuthLayout, authLayoutRoutes)}
@@ -51,6 +66,6 @@ const Routes = () => (
       />
     </Switch>
   </Router>
-);
+};
 
 export default Routes;
