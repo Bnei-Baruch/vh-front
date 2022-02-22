62ebc07c095e4125685b491322194f8d41d44d8b
Yash Shah <yashshh123@gmail.com>
2022-02-22 13:34:06 +0530

    Updated configurational changes for VH-front

---
+++ public/config/config-dev.js
@@ -0,0 +1,10 @@
+window.APP_CONFIG = {
+  LOGIN_URL: `${window.location.origin}/dash`,
+  SIGNUP_URL: `https://auth.2serv.eu/auth/realms/master/protocol/openid-connect/registrations?client_id=membership_pay_dev&response_type=code&scope=openid%20email&redirect_uri=http://eurokab.info/dash&kc_locale=en`,
+  PAYMENT_URL: `${window.location.origin}/pay/order/1`,
+  KEYCLOAK_CONFIG: {
+	realm: "master",
+	url: "https://auth.2serv.eu/auth/",
+	clientId: "membership_pay_dev"
+},
+};
