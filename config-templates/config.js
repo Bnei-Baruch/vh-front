2514b096407c68d25c8ddf759c731a57ebfcae5b
Yash Shah <yashshh123@gmail.com>
2022-02-26 00:12:53 +0530

    Update config.js

--- config-templates/config.js
+++ config-templates/config.js
@@ -3,9 +3,8 @@ window.APP_CONFIG = {
     REG_BRANCH: '{{env.Getenv "CI_COMMIT_REF_NAME" "-"}}',
     REF_VERSION: '{{env.Getenv "CI_COMMIT_SHA" "-"}}',
     LOGIN_URL:'{{env.Getenv "KEYCLOAK_DASHBOARD_URL"  "/dash"}}',
-    SIGNUP_URL: '{{env.Getenv "KEYCLOAK_SIGNUP_URL"  "https://accounts.kab.info/auth/realms/main/protocol/openid-connect/registrations?client_id=membership_pay&response_type=code&scope=openid%20email&redirect_uri=https://kli.one/dash&kc_locale=en"}}',
-    PAYMENT_URL: '{{env.Getenv "KEYCLOAK_PAYMENT_URL"  "/pay/order/1"}}',
-    PROFILE_URL: '{{env.Getenv "KEYCLOAK_PROFILE_URL" "https://api.eurokab.info/profile/v1/profile"}}',
+    SIGNUP_URL: '{{env.Getenv "KEYCLOAK_SIGNUP_URL"  "https://auth.2serv.eu/auth/realms/master/protocol/openid-connect/registrations?client_id=membership_pay_dev&response_type=code&scope=openid%20email&redirect_uri=http://eurokab.info/dash&kc_locale=en"}}',
+    PAYMENT_URL: '{{env.Getenv "PAYMENT_URL"  "/pay/order/1"}}',
     KEYCLOAK_CONFIG: {
         realm: '{{env.Getenv "KEYCLOAK_REALM" "master"}}',
         url: '{{env.Getenv "KEYCLOAK_URL" "https://auth.2serv.eu/auth/"}}',
