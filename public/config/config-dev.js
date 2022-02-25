e0d5a902a5c181eb634e09f3cbb1427a50cfb551
Yash Shah <yashshh123@gmail.com>
2022-02-26 00:01:40 +0530

    Updated configuration changes

--- public/config/config-dev.js
+++ public/config/config-dev.js
@@ -1,7 +1,7 @@
 window.APP_CONFIG = {
-  LOGIN_URL: `${window.location.origin}/dash`,
+  LOGIN_URL: `/dash`,
   SIGNUP_URL: `https://auth.2serv.eu/auth/realms/master/protocol/openid-connect/registrations?client_id=membership_pay_dev&response_type=code&scope=openid%20email&redirect_uri=http://eurokab.info/dash&kc_locale=en`,
-  PAYMENT_URL: `${window.location.origin}/pay/order/1`,
+  PAYMENT_URL: `/pay/order/1`,
   KEYCLOAK_CONFIG: {
 	realm: "master",
 	url: "https://auth.2serv.eu/auth/",
