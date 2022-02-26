84c022f9f6b0eb48cffd66d7a609c5186adfdd95
Yash Shah <yashshh123@gmail.com>
2022-02-26 12:35:22 +0530

    Updated config on the basis of feedback from Altaf
    Updated config on the basis of feedback from Altaf
    
    Need to add : SIGNUP_URL in the Gitlab Environment variable.

--- public/config/config-dev.js
+++ public/config/config-dev.js
@@ -1,7 +1,5 @@
 window.APP_CONFIG = {
-  LOGIN_URL: `/dash`,
   SIGNUP_URL: `https://auth.2serv.eu/auth/realms/master/protocol/openid-connect/registrations?client_id=membership_pay_dev&response_type=code&scope=openid%20email&redirect_uri=http://eurokab.info/dash&kc_locale=en`,
-  PAYMENT_URL: `/pay/order/1`,
   KEYCLOAK_CONFIG: {
 	realm: "master",
 	url: "https://auth.2serv.eu/auth/",
