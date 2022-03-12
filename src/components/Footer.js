8f77df024ca68f746a0f5717b8174aa0ca53dd3d
@agumnik <agumnik@gmail.com>
2022-03-12 17:05:20 +0200

    move button from broadcast to footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -30,8 +30,66 @@ const ListItem = styled(MuiListItem)`
   }
 `;
 
+const DonateSection = styled.section`
+  dir: rtl;
+  position: absolute;
+  left: ${props => props.plang === 'il' ? '59px' : 'none'};
+  right: ${props => props.plang === 'il' ? 'none' : '59px'};
+  bottom: 77px;
+`;
+
+const DonateButton = styled.button`
+    dir: ${props => props.plang === 'il' ? 'rtl' : 'ltr'};
+    background-color: #fff;
+    border: none;
+    border-radius: 24px;
+    padding: 8px 10px;
+    text-transform: uppercase;
+    box-shadow: 0px 3px 5px -1px rgba(0, 0, 0, 0.2), 0px 6px 10px rgba(0, 0, 0, 0.14), 0px 1px 18px rgba(0, 0, 0, 0.12);
+`;
+
+const DonateHeart = styled.span`
+  color: #FC6719;
+  margin-left: ${props => props.plang === 'il' ? '6px' : '4px'};
+  margin-right: ${props => props.plang === 'il' ? '4px' : '6px'};
+`;
+
+const DonateLabel = styled.span`
+  margin-right: ${props => props.plang === 'il' ? '0' : '8px'};
+  margin-left: ${props => props.plang === 'il' ? '8px' : '0'};
+`;
+
+const DonateLink = styled.a`
+  color: #1176D3;
+  text-decoration: none;
+`;
+
+const DONATE_LINKS = {
+  'il': { url: 'https://www.kab1.com/?utm_source=live_broadcast_kli_one&utm_medium=button&utm_campaign=donations&utm_id=donations&utm_term=heb&utm_content=header_button_donate'},
+  'en': { url: 'https://www.kab1.com/en?utm_source=live_broadcast_kli_one&utm_medium=button&utm_campaign=donations&utm_id=donations&utm_term=eng&utm_content=header_button_donate'},
+  'ru': { url: 'https://www.kab1.com/ru?utm_source=live_broadcast_kli_one&utm_medium=button&utm_campaign=donations&utm_id=donations&utm_term=rus&utm_content=header_button_donate'},
+  'es': { url: 'https://www.kab1.com/es?utm_source=live_broadcast_kli_one&utm_medium=button&utm_campaign=donations&utm_id=donations&utm_term=spa&utm_content=header_button_donate'},
+}
+
 function Footer() {
   const { t } = useTranslation('common');
+
+  const pageLang = localStorage.getItem("VH_LANG").toLowerCase();
+
+  const getDonateButton = () => {
+    const link = DONATE_LINKS[pageLang];
+    return (
+      <DonateButton plang={pageLang}>
+        <DonateHeart plang={pageLang}>❤</DonateHeart>
+        <DonateLabel plang={pageLang}>
+          <DonateLink href={link.url} target="_blank">
+            {t('live.donate')}
+          </DonateLink>
+        </DonateLabel>
+      </DonateButton>
+    )
+  }
+
   return (
     <Wrapper>
       <Grid container spacing={0}>
@@ -50,6 +108,9 @@ function Footer() {
             </List>
           </Grid>
         </Hidden>
+        <DonateSection plang={pageLang}>
+          {getDonateButton()}
+        </DonateSection>
       </Grid>
     </Wrapper>
   );
--- src/pages/Broadcast.js
+++ src/pages/Broadcast.js
@@ -45,44 +45,11 @@ const LiveLang = styled.span`
   padding: 0;
 `;
 
-const DonateButton = styled.button`
-    dir: ${props => props.plang === 'il' ? 'rtl' : 'ltr'};
-    background-color: #fff;
-    border: none;
-    border-radius: 24px;
-    padding: 8px 10px;
-    text-transform: uppercase;
-    box-shadow: 0px 3px 5px -1px rgba(0, 0, 0, 0.2), 0px 6px 10px rgba(0, 0, 0, 0.14), 0px 1px 18px rgba(0, 0, 0, 0.12);
-`;
-
-const DonateHeart = styled.span`
-  color: #FC6719;
-  margin-left: ${props => props.plang === 'il' ? '6px' : '4px'};
-  margin-right: ${props => props.plang === 'il' ? '4px' : '6px'};
-`;
-
-const DonateLabel = styled.span`
-  margin-right: ${props => props.plang === 'il' ? '0' : '8px'};
-  margin-left: ${props => props.plang === 'il' ? '8px' : '0'};
-`;
-
-const DonateLink = styled.a`
-  color: #1176D3;
-  text-decoration: none;
-`;
-
 const getBroadCast = (lang) => {
   return axios.post(`https://kab.tv/live/api/heartbeat`, {
     "lang": lang,
     "bitrate": 500
   }).then(res => res.data);
-}
-
-const DONATE_LINKS = {
-  'il': { url: 'https://www.kab1.com/?utm_source=live_broadcast_kli_one&utm_medium=button&utm_campaign=donations&utm_id=donations&utm_term=heb&utm_content=header_button_donate'},
-  'en': { url: 'https://www.kab1.com/en?utm_source=live_broadcast_kli_one&utm_medium=button&utm_campaign=donations&utm_id=donations&utm_term=eng&utm_content=header_button_donate'},
-  'ru': { url: 'https://www.kab1.com/ru?utm_source=live_broadcast_kli_one&utm_medium=button&utm_campaign=donations&utm_id=donations&utm_term=rus&utm_content=header_button_donate'},
-  'es': { url: 'https://www.kab1.com/es?utm_source=arvut_app&utm_medium=button&utm_campaign=donations&utm_id=donations&utm_term=spa&utm_content=button_donate'},
 }
 
 export default function Broadcast() {
@@ -115,22 +82,6 @@ export default function Broadcast() {
 
   const pageLang = localStorage.getItem("VH_LANG").toLowerCase();
 
-  console.log(pageLang);
-
-  const getDonateButton = () => {
-    const link = DONATE_LINKS[pageLang];
-    return (
-      <DonateButton plang={pageLang}>
-        <DonateHeart plang={pageLang}>❤</DonateHeart>
-        <DonateLabel plang={pageLang}>
-          <DonateLink href={link.url} target="_blank">
-            {t('live.donate')}
-          </DonateLink>
-        </DonateLabel>
-      </DonateButton>
-    )
-  }
-
   return (
     <>
       <Helmet title={t('live.name')} />
@@ -161,9 +112,6 @@ export default function Broadcast() {
                       </Select>
                     </FormControl>
                 </LanguageContainerLeftPart>
-                <section className="donate">
-                  {getDonateButton()}
-                </section>
               </LanguageContainer>
               <Grid item xs={12} sm={12}>
                 <ReactHlsPlayer
