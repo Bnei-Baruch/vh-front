0a65ddeea22db10196104685377a2a9012e1583d
Edo Shor <edoshor@gmail.com>
2025-07-13 09:08:01 +0300

    bump keycloak-js, react-scripts and some cleanup

--- config-overrides.js
+++
@@ -1,8 +0,0 @@
-/* Doc : https://material-ui.com/guides/minimizing-bundle-size/  */
-/* config-overrides.js */
-
-const { useBabelRc, override } = require('customize-cra')
-
-module.exports = override(
-  useBabelRc()
-);
