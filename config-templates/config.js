8cb5618f79f447722a35df45a23b9720dbc5a0fd
Edo Shor <edoshor@gmail.com>
2026-03-21 13:37:09 +0200

    Add GitHub Actions workflow and update config template

--- config-templates/config.js
+++ config-templates/config.js
@@ -1,7 +1,5 @@
 window.APP_CONFIG = {
-    BUILD_TIME: '{{ (time.Now).UTC }}',
-    REG_BRANCH: '{{env.Getenv "CI_COMMIT_REF_NAME" "-"}}',
-    REF_VERSION: '{{env.Getenv "CI_COMMIT_SHA" "-"}}',
+    SHORT_SHA: '{{env.Getenv "SHORT_SHA" "-"}}',
     SIGNUP_URL: '{{env.Getenv "KEYCLOAK_SIGNUP_URL"  "https://auth.2serv.eu/auth/realms/master/protocol/openid-connect/registrations?client_id=membership_pay_dev&response_type=code&scope=openid%20email&redirect_uri=http://eurokab.info/dash&kc_locale=en"}}',
     KEYCLOAK_CONFIG: {
         realm: '{{env.Getenv "KEYCLOAK_REALM" "master"}}',
