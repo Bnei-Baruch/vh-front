9ea0fb5f98e67587db367e6da50d7f19b8b00f2a
Edo Shor <edoshor@gmail.com>
2026-08-04 05:21:36 +0300

    Update config.js

--- config-templates/config.js
+++ config-templates/config.js
@@ -1,9 +1,9 @@
 window.APP_CONFIG = {
     SHORT_SHA: '{{env.Getenv "SHORT_SHA" "-"}}',
-    SIGNUP_URL: '{{env.Getenv "KEYCLOAK_SIGNUP_URL"  "https://auth.2serv.eu/auth/realms/master/protocol/openid-connect/registrations?client_id=membership_pay_dev&response_type=code&scope=openid%20email&redirect_uri=http://eurokab.info/dash&kc_locale=en"}}',
+    SIGNUP_URL: '{{env.Getenv "KEYCLOAK_SIGNUP_URL"  "https://accounts.kab.sh/auth/realms/main/protocol/openid-connect/registrations?client_id=membership_pay&response_type=code&scope=openid%20email&redirect_uri=https://staging-vh.kli.one/dash&kc_locale=en"}}',
     KEYCLOAK_CONFIG: {
-        realm: '{{env.Getenv "KEYCLOAK_REALM" "master"}}',
-        url: '{{env.Getenv "KEYCLOAK_URL" "https://auth.2serv.eu/auth/"}}',
-        clientId: '{{env.Getenv "KEYCLOAK_CLIENT_ID" "membership_pay_dev"}}'
+        realm: '{{env.Getenv "KEYCLOAK_REALM" "main"}}',
+        url: '{{env.Getenv "KEYCLOAK_URL" "https://accounts.kab.sh/auth/"}}',
+        clientId: '{{env.Getenv "KEYCLOAK_CLIENT_ID" "membership_pay"}}'
     }
 };
